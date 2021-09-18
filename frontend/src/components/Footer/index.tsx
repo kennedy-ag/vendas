f610e87104321f1829dd7b06b7bcd304b9a1d54c
Kennedy <kennedycontato46@gmail.com>
2021-09-18 17:31:02 -0300

    Criados alguns componentes

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/kennedy-ag" target="_blank" rel="noreferrer">Kennedy Viana Aguiar</a></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
